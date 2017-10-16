b2029f7bf3cae871b532b1a75e6c7b5b22e32ec5
vjiang <v-vjiang@expedia.com>
2017-10-16 16:57:20 +0800

    update

--- src/wzry/main_demo.py
+++ src/wzry/main_demo.py
@@ -6,19 +6,18 @@ def start_wzry_driver():
     desired_caps={}
     desired_caps['deviceName']='df836a957d73'
     desired_caps['platformName'] = 'Android'
-    desired_caps['androidPackage'] = 'com.tencent.tmgp.sgame'
+    desired_caps['appPackage'] = 'com.tencent.tmgp.sgame'
     desired_caps['appActivity'] = '.SGameActivity'
     desired_caps['newCommandTimeout']='600'
     return webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
 
-
+driver=start_wzry_driver()
 try:
-    driver=start_wzry_driver()
     print('start to wait')
-    time.sleep(20)
     print('start to find pic')
-    point=click_start_game_position(driver)
+    click_start_game_position(driver)
+    click_close_window_normal(driver)
     # prepare_steps(driver)
 
     print('hhhh')
--- src/wzry/page/use_steps.py
+++ src/wzry/page/use_steps.py
@@ -1,9 +1,10 @@
 from name_steps import *
 from src.wzry.utils.page_utils import *
 import time
+from src.wzry.exception.TimeOutException import *
 
 
-def __wait_pic(driver,name,wait_time=5,buffer_time=0.5):
+def __wait_pic__(driver,name,wait_time=5,buffer_time=0.5):
     point=(-1,-1)
     print('used to add:'+str(time.time()))
     end_time=time.time()+float(wait_time)
@@ -18,14 +19,28 @@ def __wait_pic(driver,name,wait_time=5,buffer_time=0.5):
         time.sleep(buffer_time)
         print('now_end:'+str(time.time()))
     if point==(-1,-1):
-        raise Exception('wait '+name+' failed! please check')
+        raise TimeOutException('wait '+name+' failed! please check')
     return point
+
+def __wait_click_pic_circulation(driver,name,wait_time=30,buffer_time=1):
+    point=(-1,-1)
+    end_time=time.time()+float(wait_time)
+    while(time.time()<end_time):
+        try:
+            point=__wait_pic__(driver,name)
+        except TimeOutException as e:
+            pass
+        if point!=(-1,-1):
+            driver.tap([point],500)
+            time.sleep(buffer_time)
 
 
 def click_start_game_position(driver):
-    point= __wait_pic(driver,START_GAME,20)
+    point= __wait_pic__(driver,START_GAME,3000,2)
     driver.tap([point],500)
 
+def click_close_window_normal(driver):
+    __wait_click_pic_circulation(driver,CLOSE_WINDOW)
 
 
 def click_battle_position():
